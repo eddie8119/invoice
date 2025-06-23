4e464118191f529c74bd97d6a2ff03a70cccc9de
eddie wang <wang8119@gmail.com>
2025-06-24 01:16:33 +0800

    chore: sever

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
+import routes from "@/routes/index.js";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import routes from "./routes";
 
 dotenv.config();
 
@@ -10,9 +10,9 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(routes);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import authRoutes from "./auth";
-import ocrRoutes from "./ocr";
+import authRoutes from "./auth.js";
+import ocrRoutes from "./ocr.js";
 
 const app = express();
 
-app.use("/api/ocr", ocrRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/ocr", ocrRoutes);
 
 export default app;
