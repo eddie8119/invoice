5927d78b185b3001e6a18c58920c73986906bd48
eddie wang <wang8119@gmail.com>
2025-06-27 23:59:11 +0800

    chore: client add share path

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = function (api) {
   api.cache(true);
   return {
@@ -5,6 +7,18 @@ module.exports = function (api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './',
+            '@shared': path.resolve(__dirname, '../shared'),
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        },
+      ],
+    ],
   };
 };
