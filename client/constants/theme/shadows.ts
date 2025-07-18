2d43e8fb12184a0f779c5f052ab86dc062c7fe23
LAPTOP-CAH83BQA\eddie <wang8119@gmail.com>
2025-07-18 17:33:57 +0800

    shadow rename

--- client/constants/theme/shadows.ts
+++ client/constants/theme/shadows.ts
@@ -1,25 +1,14 @@
-import { shadow } from './color';
-
 export const shadows = {
   small: {
-    shadowColor: shadow.main,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.15,
-    shadowRadius: 3,
+    boxShadow: '0px 2px 3px rgba(0, 0, 0, 0.15)',
     elevation: 2,
   },
   medium: {
-    shadowColor: shadow.main,
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
+    boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.3)',
     elevation: 5,
   },
   large: {
-    shadowColor: shadow.main,
-    shadowOffset: { width: 0, height: 6 },
-    shadowOpacity: 0.35,
-    shadowRadius: 8,
+    boxShadow: '0px 6px 8px rgba(0, 0, 0, 0.35)',
     elevation: 8,
   },
 } as const;
